55299bae0d25edcb32eda24b961b381dd63dfb89
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-09-23 13:55:22 -0700

    Completed js for loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var num = 0;
+for (let i = 0; i < 10; i++) {
+  console.log(num);
+  num += 2;
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
